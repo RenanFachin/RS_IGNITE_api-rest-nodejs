bb5df86690496acef0691e2a57bf91fb60b7798c
Renan <renan.fachin@hotmail.com>
2023-04-13 15:29:15 -0300

    Estabelecendo a conexão com db - SQLITE e KNEX

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello world'
+app.get('/hello', async () => {
+  // Testando a configuração do database
+  // Todo db possui uma tabela chamada sqlite_schema e dentro dela existe infos sobre as outras tabelas
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
