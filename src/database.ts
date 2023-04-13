23db7cb81999668ab794436da9ad619e3c43879a
Renan <renan.fachin@hotmail.com>
2023-04-13 15:59:37 -0300

    Configurações iniciais para migrations no knex com typescript

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,17 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+// : Knex.Config serve para tipar o objeto de configurações
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './database/app.db',
   },
-})
+  useNullAsDefault: true,
+  // Configurações das migrations
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
