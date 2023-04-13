dd955c6c26105257bb6f5af135986adc9edaa052
Renan <renan.fachin@hotmail.com>
2023-04-13 00:57:04 -0300

    Configura esLint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
-
 
 app.get('/hello', () => {
   return 'Hello world'
 })
 
-
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
